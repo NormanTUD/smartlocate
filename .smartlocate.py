3bc834d50f2f3984c6a75187f356bbbe686fa446
n <norman.koch@tu-dresden.de>
2025-01-07 06:13:02 +0100

    fix

--- .smartlocate.py
+++ .smartlocate.py
@@ -1507,9 +1507,15 @@ def main() -> None:
 
         if args.face_recognition:
             if supports_sixel():
-                c = 1
+                face_recognition_images = []
+
                 for image_path in image_paths:
-                    console.print(f"Face recognition: {c}/{len(image_paths)}")
+                    if not faces_already_recognized(conn, image_path): 
+                        face_recognition_images.append(image_path)
+
+                c = 1
+                for image_path in face_recognition_images:
+                    console.print(f"Face recognition: {c}/{len(face_recognition_images)}")
                     if not faces_already_recognized(conn, image_path): 
                         file_size = os.path.getsize(image_path)
 
